e3bacec39c878fc70786c44bc8e390b9304b3840
Cody <cy@wantering.com>
2013-02-09 19:04:12 -0800

    Changed scraper to include unit when grabbing background-position

--- js/berrymotes.js
+++ js/berrymotes.js
@@ -19,15 +19,17 @@ var apply_emotes = function (chat_message) {
             if (emote) {
                 emote = berryemotes[emote];
                 if(show_nsfw_emotes === false && emote.nsfw) continue;
-                var pos = (emote['background-position'] || [0, 0] );
+                var pos = (emote['background-position'] || ['0px', '0px']);
                 position_string = '';
                 for (var a = 0; a < pos.length; ++a) {
-                    position_string += pos[a] + "px ";
+                    position_string += pos[a];
                 }
                 emote['position_string'] = position_string;
-                //var emote_dom = $('<span class="berry_emote"></span>');
                 var emote_code =
-                    ['<span style="',
+                    ['<span class="berryemote',
+                        emote.height > max_emote_height ? 'resize' : '',
+                        '" ',
+                        'style="',
                         'background-image: url(', emote['background-image'], '); ',
                         'height:', emote.height, 'px; ',
                         'width:', emote.width, 'px; ',
@@ -40,7 +42,31 @@ var apply_emotes = function (chat_message) {
                 chat_message = chat_message.replace(replace_regex, emote_code);
             }
         }
+        chat_message = $(chat_message);
+        var emotes_to_resize = chat_message.find('.resize');
+        $.each(emotes_to_resize, function(i, emote){
+            var $emote = $(emote);
+            var scale =  max_height / $emote.height();
+            var innerwrap = $emote.wrap('<div class="emote-wrapper"><div class="emote-wrapper"></div></div>').parent();
+            var outerwrap = innerwrap.parent();
+            outerwrap.css('height', $emote.height() * scale);
+            outerwrap.css('width', $emote.width() * scale);
+            outerwrap.css('display', 'inline-block');
+            outerwrap.css('position', 'relative');
+            innerwrap.css('transform', ['scale(', scale, ', ', scale, ')'].join(''));
+            innerwrap.css('transform-origin', 'left top');
+            innerwrap.css('position', 'absolute');
+            innerwrap.css('top', '0');
+            innerwrap.css('left', '0');
+        });
+        return chat_message;
     }
+    else
+    {
+        var chat_message = $('<span/>').html(chat_message);
+    }
+    var re = RegExp("^>");
+    if(chat_message.text().match(re)) chat_message.addClass("green");
     return chat_message;
 };
 
@@ -57,7 +83,7 @@ var monkey_patch = function () {
 };
 
 function wait_to_start() {
-    if (typeof formatChatMsg === "undefined") {
+    if (typeof formatChatMsg === "undefined" || typeof berryemotes === "undefined") {
         setTimeout(wait_to_start, 100);
         console.log('waiting ');
     }
