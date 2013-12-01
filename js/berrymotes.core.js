6629b7fe707f06c8da4d400e6d7edd90e59a91d5
berrycardsagainsthumanity <berrycardsagainsthumanity@gmail.com>
2013-12-01 13:44:07 -0800

    Licenses! [](/celestiyay-slide)

--- js/berrymotes.core.js
+++ js/berrymotes.core.js
@@ -1,3 +1,15 @@
+/*
+ * Copyright (C) 2013 Cody <cody_y@shaw.ca>
+ * Copyright (C) 2013 Marminator <cody_y@shaw.ca>
+ * Copyright (C) 2013 Patrick O'Leary <patrick.oleary@gmail.com>
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * COPYING for more details.
+ */
+
 Bem = typeof Bem === "undefined" ? {} : Bem;
 (function ($) {
     var settings_schema = [
