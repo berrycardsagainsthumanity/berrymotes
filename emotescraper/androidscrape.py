372701731fd3ee1c20b3149e6498fb3134133cf5
Daniel Triendl <daniel@pew.cc>
2014-07-20 17:52:23 +0200

    Save emote list on a per subreddit basis to allow the app to only load the emote list of subreddits that are going to get synced.
    Emote lists are now only updated if new emotes are added, allowing
    the app to abort the download and parsing of the list if nothing
    has changed.

--- emotescraper/androidscrape.py
+++ emotescraper/androidscrape.py
@@ -21,7 +21,6 @@ import os
 import gzip
 
 factory = AndroidEmotesProcessorFactory(single_emotes_filename=os.path.join( '..', 'single_emotes', '{}', '{}.png'))
-
 scraper = BMScraper(factory)
 scraper.user = 'ponymoteharvester'
 scraper.password = 'berry_punch'
@@ -35,4 +34,25 @@ scraper.scrape()
 
 f = gzip.open(os.path.join('..', 'single_emotes', 'emotes.json.gz'), 'wb')
 f.write(dumps(factory.emotes, separators=(',', ': ')))
+f.close()
+
+for subreddit in subreddits:
+    subreddit_emotes = [x for x in factory.emotes if x['sr'] == subreddit]
+    subreddit_emotes = sorted(subreddit_emotes, key = lambda x: x['image'])
+    emotes_file = os.path.join('..', 'single_emotes', subreddit, 'emotes.json.gz')
+    if not os.path.exists(os.path.dirname(emotes_file)):
+        os.makedirs(os.path.dirname(emotes_file))
+        
+    emotes_data = dumps(subreddit_emotes, separators=(',', ': '));
+    f = gzip.open(emotes_file, 'r')
+    emotes_data_old = f.read()
+    f.close()
+    
+    if emotes_data != emotes_data_old:
+        f = gzip.open(emotes_file, 'wb')    
+        f.write(emotes_data)
+        f.close()
+    
+f = gzip.open(os.path.join('..', 'single_emotes', 'subreddits.json.gz'), 'wb')
+f.write(dumps(subreddits, separators=(',', ': ')))
 f.close()
